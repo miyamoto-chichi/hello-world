8ed743883f68c18edba534adfcaddb2eb44bd976
miyamoto <miyamoto.chichi@gmail.com>
2018-10-03 20:05:53 +0900

    local test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello GraphQL！</h1>, document.getElementById('app'));
+ReactDOM.render(<h1>Hello GraphQL！ Local Test.</h1>, document.getElementById('app'));
